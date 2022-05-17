8c1527982a835fdddce783c281ceb5d7517d2fa1
Junchao Wu <wujunchaoIU@outlook.com>
2022-05-17 14:45:44 +0800

    add Trie Tree structure

---
+++ qcnlp/data_structure/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding=utf-8 -*-
+"""
+# library: qcnlp
+# author: wujunchao
+# license: Apache License 2.0
+# email: wujunchaoIU@outlook.com
+# github: https://github.com/junchaoIU/QCNLP
+# description: A Preprocessing & Parsing tool for Chinese Natural Language Processing
+"""
